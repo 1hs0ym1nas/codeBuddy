fbf5e41def26bb6ae86daf55249b31d524289b01
Minghui <xu.minghu@northeastern.edu>
2024-11-28 14:51:46 -0800

    Initial Commit

---
+++ team39_FinalProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  team39_FinalProject
+//
+//  Created by Minghui Xu on 11/28/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
